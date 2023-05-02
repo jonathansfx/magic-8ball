27ecd7833acda7abc7c58450615d5901a92a59bb
jonathan <97814319+jonathansfx@users.noreply.github.com>
2023-05-01 18:12:21 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,33 @@
-// Btn Event Listener
 document.getElementById("btn").addEventListener("click", btnClicked);
 
 function btnClicked() {
-  // HTML Var
-  question = document.getElementById("input").value.toLowerCase();
+  const question = document.getElementById("input").value.toLowerCase();
+  checkQuestion(question);
+}
 
-  checkQuestion();
-  randomResponse();
-
-  // Functions
-  function checkQuestion(question) {
-    if (question == "") {
-      output.innerHTML = "Please ask a question...";
-    } else if (question == "does a magic 8 ball actually work?") {
-      output.innerHTML = "How dare you doubt me! ";
-    }
+function checkQuestion(question) {
+  const output = document.getElementById("output");
+  if (question === "") {
+    output.innerHTML = "Please ask a question...";
+  } else if (question === "does a magic 8 ball actually work?") {
+    output.innerHTML = "How dare you doubt me!";
+  } else {
+    randomResponse();
   }
+}
 
-  function randomResponse() {
-    let rand = Math.random();
-    if (rand <= 0.2) {
-      output.innerHTML = "Without a Doubt.";
-    } else if (rand <= 0.4) {
-      output.innerHTML = "As I see it, yes.";
-    } else if (rand <= 0.6) {
-      output.innerHTML = "Concentrate and ask again.";
-    } else if (rand <= 0.8) {
-      output.innerHTML = "Don't count on it";
-    } else output.innerHTML = "Outlook not so good.";
+function randomResponse() {
+  const output = document.getElementById("output");
+  let rand = Math.random();
+  if (rand <= 0.2) {
+    output.innerHTML = "Without a Doubt.";
+  } else if (rand <= 0.4) {
+    output.innerHTML = "As I see it, yes.";
+  } else if (rand <= 0.6) {
+    output.innerHTML = "Concentrate and ask again.";
+  } else if (rand <= 0.8) {
+    output.innerHTML = "Don't count on it";
+  } else {
+    output.innerHTML = "Outlook not so good.";
   }
 }
